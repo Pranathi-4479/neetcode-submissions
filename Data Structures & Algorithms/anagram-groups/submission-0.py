44b3308fec42a47f9dc405737436f18ceb11f016
Vadapalli Lakshmi Pranathi <pranathivadapalli460@gmail.com>
2026-08-09 09:49:55 +0530

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.py
@@ -0,0 +1,10 @@
+class Solution:
+    def groupAnagrams(self, s: List[s]) -> List[List[s]]:
+        hash_map = {}
+        for val in s:
+            key = "".join(sorted(val))
+            if key in hash_map:
+                hash_map[key].append(val)
+            else:
+                hash_map[key] = [val]
+        return  list(hash_map.values())
