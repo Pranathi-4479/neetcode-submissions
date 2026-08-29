07d2a49c323ddb049f6f213908c52b40eebe5499
Vadapalli Lakshmi Pranathi <pranathivadapalli460@gmail.com>
2026-08-29 18:01:43 +0530

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.py
@@ -0,0 +1,22 @@
+# class Solution:
+#     def maxProfit(self, prices: List[int]) -> int:
+#         buy=min(prices)
+#         i=prices.index(buy)
+#         sell=buy
+#         for j in range(i+1,len(prices)):
+#             if prices[j]>sell:
+#                 sell=prices[j]
+#         return sell-buy
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        min_price = float('inf')
+        max_profit = 0
+        
+        for price in prices:
+            if price < min_price:
+                min_price = price
+            elif price - min_price > max_profit:
+                max_profit = price - min_price
+                
+        return max_profit
+        
